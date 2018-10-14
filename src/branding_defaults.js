d1eeeea14c360a3a14bcbd057f050e3c8135e76c
garrettmac <5916111+garrettmac@users.noreply.github.com>
2018-10-13 19:02:23 -0700

    Update branding_defaults.js

--- src/branding_defaults.js
+++ src/branding_defaults.js
@@ -1,21 +1,21 @@
 export default {
-  textColor: "#000",
-  accentColor: "red",
-  alternateTextColor: "#fff",
+  textColor: '#000',
+  accentColor: 'red',
+  alternateTextColor: '#fff',
 
-  linkColor: "#7945ef",
+  linkColor: '#565656',
 
-  disabledColor: "#f9f9fa",
-  disabledDarkColor: "#b1b2c1",
+  disabledColor: '#f9f9fa',
+  disabledDarkColor: 'rgba(149,149,149,.6)',
 
-  successLightColor: "#e9f9f0",
-  successColor: "#20cd68",
+  successLightColor: '#e9f9f0',
+  successColor: '#20cd68',
 
-  passiveSuccessLightColor: "#d5dff7",
-  passiveSuccessColor: "#2f61d5",
+  passiveSuccessLightColor: '#d5dff7',
+  passiveSuccessColor: '#2f61d5',
 
-  dangerLightColor: "#ffe2dc",
-  dangerColor: "#ff7052",
-  primaryLightColor: "#e3d9fc",
-  primaryColor: "#7540ee"
+  dangerLightColor: '#ffe2dc',
+  dangerColor: '#ff7052',
+  primaryLightColor: '#ff6868',
+  primaryColor: '#c13538',
 };
